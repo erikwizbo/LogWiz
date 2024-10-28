8fe86788c8c48863643f7bbffbc1d7ab82237e79
erikwizbo <82509929+erikwizbo@users.noreply.github.com>
2024-10-28 18:55:24 -0400

    Update Convert_to_JSON.ps1

--- Convert_to_JSON.ps1
+++ Convert_to_JSON.ps1
@@ -1,10 +1,11 @@
-# Define paths with an absolute path to the .evtx file
+#define paths to evtx file, output folder, file name, and path
+
 $inputFilePath = "C:\Users\jackw\OneDrive\Documents\ComputerSecurity\computerlogs_security.evtx"
 $outputFolder = "C:\Users\jackw\OneDrive\Documents\ComputerSecurity\ConvertedLogs"
 $outputFileName = "computerlogs_security.json"
 $outputFilePath = Join-Path -Path $outputFolder -ChildPath $outputFileName
 
-# Check if the .evtx file exists
+#check if the .evtx file exists
 if (!(Test-Path -Path $inputFilePath)) {
     Write-Output "Error: Input file not found at $inputFilePath"
     exit
@@ -12,12 +13,12 @@ if (!(Test-Path -Path $inputFilePath)) {
     Write-Output "File found at $inputFilePath, proceeding with conversion..."
 }
 
-# Ensure the output folder exists
+#make sure output folder exists
 if (!(Test-Path -Path $outputFolder)) {
     New-Item -ItemType Directory -Path $outputFolder
 }
 
-# Convert .evtx to JSON and save to output folder
+#convert evtx to JSON and save to path
 try {
     Get-WinEvent -FilterHashtable @{LogName="Security"} | ForEach-Object {
         $_ | Select-Object TimeCreated, Id, LevelDisplayName, Message, ProviderName | ConvertTo-Json -Compress
